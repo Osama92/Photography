2ed813f66d451cbe154076ddfadaff7bb25a53e7
Osama92 <try2rym@yahoo.com>
2021-08-30 22:18:15 +0100

    Added Scroll Animation

---
+++ animation.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.camera1', {duration: 1000, delay:500, opacity: 0,distance: '70px', origin: 'right'});
+ScrollReveal().reveal('.camImg', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'left'});
+ScrollReveal().reveal('.camera2', {duration: 1000, delay:500, opacity: 0,distance: '70px', origin: 'top'});
+ScrollReveal().reveal('.s1, .s2, .s3, .s4', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'bottom', interval: 300});
+ScrollReveal().reveal('#portfolio', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'right', interval: 300});
+ScrollReveal().reveal('.heroHead', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'top'});
+ScrollReveal().reveal('.heroMiddle', {duration: 1000, delay:700, opacity: 0});
