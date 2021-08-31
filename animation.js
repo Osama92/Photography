4ba611f3525dac9060e0fe07bedbec41a6117ddf
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-08-31 18:55:12 +0100

    Minor Changes

--- animation.js
+++ animation.js
@@ -1,10 +1,80 @@
-ScrollReveal().reveal('.camera1', {duration: 1000, delay:500, opacity: 0,distance: '70px', origin: 'right'});
-ScrollReveal().reveal('.camImg', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'left'});
-ScrollReveal().reveal('.camera2', {duration: 1000, delay:500, opacity: 0,distance: '70px', origin: 'top'});
-ScrollReveal().reveal('.s1, .s2, .s3, .s4', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'bottom', interval: 300});
-ScrollReveal().reveal('#portfolio', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'right', interval: 300});
-ScrollReveal().reveal('.heroHead', {duration: 1000, delay:400, opacity: 0,distance: '70px', origin: 'top'});
-ScrollReveal().reveal('.heroMiddle', {duration: 1000, delay:700, opacity: 0});
-ScrollReveal().reveal('.readMore', {duration: 1000, delay:400, opacity: 0, distance: '70px', origin: 'bottom'});
-ScrollReveal().reveal('.portContact', {duration: 1000, delay:1000, opacity: 0, distance: '70px', origin: 'left'});
-ScrollReveal().reveal('#modelImage', {duration: 1000, delay:900, opacity: 0, distance: '-70px', origin: 'right'});
+ScrollReveal().reveal(".camera1", {
+  duration: 1000,
+  delay: 500,
+  opacity: 0,
+  distance: "70px",
+  origin: "right",
+  reset: true
+});
+ScrollReveal().reveal(".camImg", {
+  duration: 1000,
+  delay: 400,
+  opacity: 0,
+  distance: "70px",
+  origin: "left",
+  reset: true
+});
+ScrollReveal().reveal(".camera2", {
+  duration: 1000,
+  delay: 500,
+  opacity: 0,
+  distance: "70px",
+  origin: "top",
+  reset: true
+});
+ScrollReveal().reveal(".s1, .s2, .s3, .s4", {
+  duration: 1000,
+  delay: 400,
+  opacity: 0,
+  distance: "70px",
+  origin: "bottom",
+  interval: 300,
+  reset: true
+});
+ScrollReveal().reveal("#portfolio", {
+  duration: 1000,
+  delay: 400,
+  opacity: 0,
+  distance: "70px",
+  origin: "right",
+  interval: 300,
+  reset: true
+});
+ScrollReveal().reveal(".heroHead", {
+  duration: 1000,
+  delay: 400,
+  opacity: 0,
+  distance: "70px",
+  origin: "top",
+  reset: true
+});
+ScrollReveal().reveal(".heroMiddle", {
+  duration: 1000,
+  delay: 700,
+  opacity: 0,
+  reset: true
+});
+ScrollReveal().reveal(".readMore", {
+  duration: 1000,
+  delay: 400,
+  opacity: 0,
+  distance: "70px",
+  origin: "bottom",
+  reset: true
+});
+ScrollReveal().reveal(".portContact", {
+  duration: 1000,
+  delay: 1000,
+  opacity: 0,
+  distance: "70px",
+  origin: "left",
+  reset: true
+});
+ScrollReveal().reveal("#modelImage", {
+  duration: 1000,
+  delay: 900,
+  opacity: 0,
+  distance: "-70px",
+  origin: "right",
+  reset: true
+});
