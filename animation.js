7d3be8fd080548b713c28f44bf513a87d23ad742
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-01 13:44:51 +0100

    Working on NewsLetter Tab

--- animation.js
+++ animation.js
@@ -3,24 +3,21 @@ ScrollReveal().reveal(".camera1", {
   delay: 500,
   opacity: 0,
   distance: "70px",
-  origin: "right",
-  reset: true
+  origin: "right"
 });
 ScrollReveal().reveal(".camImg", {
   duration: 1000,
   delay: 400,
   opacity: 0,
   distance: "70px",
-  origin: "left",
-  reset: true
+  origin: "left"
 });
 ScrollReveal().reveal(".camera2", {
   duration: 1000,
   delay: 500,
   opacity: 0,
   distance: "70px",
-  origin: "top",
-  reset: true
+  origin: "top"
 });
 ScrollReveal().reveal(".s1, .s2, .s3, .s4", {
   duration: 1000,
@@ -28,8 +25,7 @@ ScrollReveal().reveal(".s1, .s2, .s3, .s4", {
   opacity: 0,
   distance: "70px",
   origin: "bottom",
-  interval: 300,
-  reset: true
+  interval: 300
 });
 ScrollReveal().reveal("#portfolio", {
   duration: 1000,
@@ -37,44 +33,38 @@ ScrollReveal().reveal("#portfolio", {
   opacity: 0,
   distance: "70px",
   origin: "right",
-  interval: 300,
-  reset: true
+  interval: 300
 });
 ScrollReveal().reveal(".heroHead", {
   duration: 1000,
   delay: 400,
   opacity: 0,
   distance: "70px",
-  origin: "top",
-  reset: true
+  origin: "top"
 });
 ScrollReveal().reveal(".heroMiddle", {
   duration: 1000,
   delay: 700,
-  opacity: 0,
-  reset: true
+  opacity: 0
 });
 ScrollReveal().reveal(".readMore", {
   duration: 1000,
   delay: 400,
   opacity: 0,
   distance: "70px",
-  origin: "bottom",
-  reset: true
+  origin: "bottom"
 });
 ScrollReveal().reveal(".portContact", {
   duration: 1000,
   delay: 1000,
   opacity: 0,
   distance: "70px",
-  origin: "left",
-  reset: true
+  origin: "left"
 });
 ScrollReveal().reveal("#modelImage", {
   duration: 1000,
   delay: 900,
   opacity: 0,
   distance: "-70px",
-  origin: "right",
-  reset: true
+  origin: "right"
 });
